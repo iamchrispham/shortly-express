cce8bad082bc90d4699895a3ba5cf57c91745eac
iamchrispham <chrismpham@gmail.com>
2019-06-21 20:15:10 -0700

    Finish cookie parser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,21 @@
+const createSession = require('./auth');
+
 const parseCookies = (req, res, next) => {
+	var cookies = {};
+	console.log('REq ', req);
+	if (req.headers.cookie) {
+		var newCookies = req.headers.cookie.split("; ");
+		for (var i = 0; i < newCookies.length; i++) {
+			var splitCookie = newCookies[i].split('=');
+			console.log('cookie value ', splitCookie[1])
+			cookies[splitCookie[0]] = splitCookie[1];
+		}
+	}
+	req.cookies = cookies;
+	console.log('RQ ' + req.cookies)
+	console.log('RS ' + res.cookies)
+
+	next();
 };
 
 module.exports = parseCookies;
